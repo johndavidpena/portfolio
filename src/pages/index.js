c5790f27ffc6a8a4abce9a0ddc2846f810ab6e33
John Pena <pemu775@gmail.com>
2019-04-15 15:57:13 -0400

    Styling the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import '../components/index.css'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,13 +9,18 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Home__top">
+      <h1 className="Home__title">JOHN<span className="Home__title-period">.</span></h1>
+      <p className="Home__who">Hi, my name is John Pena and I make websites.</p>
+      <p className="Home__who">As <span className="Home__who-mimas">Mimas the moon</span>, I make music.</p>
+      <p className="Home__who">I lovingly created and maintain <a className="Home__who-link" href="www.mimasmusic.com" className="Home__who-link" target="_blank">mimasmusic</a></p>
+      <p className="Home__see">Check out some of my work below <span className="Home__see-down">&#8623;</span></p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div> */}
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
