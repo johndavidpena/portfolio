f2df5e3590b54b7142e86b5bc08bc0ada93fb20a
John Pena <pemu775@gmail.com>
2019-09-09 10:20:55 -0600

    Added dharmanure site

--- src/SitesInfo.js
+++ src/SitesInfo.js
@@ -1,10 +1,19 @@
 import CafeImage from './images/cafeImage.jpg'
 import FeedinImage from './images/feedinImage.png'
-// import MimasImage from './images/mimasImage.jpg'
+import DharmanureImage from './images/dharmanureImage.png';
 
 const SitesInfo = [
   {
     id: 1,
+    title: 'Dharmanure',
+    sub: 'Headless CMS',
+    tools: 'React, Gatsby, Contentful',
+    image: DharmanureImage,
+    alt: 'The dharmanure homepage',
+    link: 'https://dharmanure.com'
+  },
+  {
+    id: 2,
     title: 'Cafe Racer',
     sub: 'Modern Commerce Design',
     tools: 'HTML, CSS, javascript',
@@ -13,7 +22,7 @@ const SitesInfo = [
     link: 'https://caferacer.netlify.com'
   },
   {
-    id: 2,
+    id: 3,
     title: "Feedin' Time",
     sub: 'Mobile Web App',
     tools: 'React and Firebase',
